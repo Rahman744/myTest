ea7e0ad8f45792ecb814a47681b51fda427f521f
unknown <User@DESKTOP-IT11.simurg.local>
2025-06-16 16:58:03 +0500

    1.0.2 commit

--- resources/views/first.blade.php
+++ resources/views/first.blade.php
@@ -13,11 +13,20 @@
 
     <form method="POST" action="/start" class="text-center w-50">
         @csrf
+        @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+        @endif
+
         <div class="mb-3">
             <input type="text" name="name" class="form-control" placeholder="Your name" required>
         </div>
-        <div class="mb-3">
+        <!-- <div class="mb-3">
             <input type="email" name="email" class="form-control" placeholder="Your email" required>
+        </div> -->
+        <div class="mb-3">
+            <input type="password" name="code" class="form-control" placeholder="Password" required>
         </div>
         <button type="submit" class="btn btn-primary">Start</button>
     </form>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
 
 Route::get('/', function () {
     return view('first');
 });
 
-Route::post('/start', function () {
-    session(['name' => request('name')]);
+Route::post('/start', function (Request $request) {
+    $name = $request->input('name');
+    $code = $request->input('code');
+
+    if ($code !== '1234') {
+        return redirect('/')
+            ->with('error', 'Wrong code!')
+            ->withInput();
+    }
+
+    session(['name' => $name]);
+    session(['access_granted' => true]);
     return redirect('/1');
 });
+
 
 Route::get('/{id}', function ($id) {
     $name = session('name');
